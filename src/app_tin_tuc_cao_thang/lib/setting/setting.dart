c052c425f980baf8ff38099d6435ee4e4a6adae8
pxthinh.vn@gmail.com <pxthinh.vn@gmail.com>
2022-06-10 19:04:25 +0700

    form setting

--- src/app_tin_tuc_cao_thang/lib/setting/setting.dart
+++ src/app_tin_tuc_cao_thang/lib/setting/setting.dart
@@ -16,33 +16,109 @@ class Setting extends StatefulWidget {
 class _SettingState extends State<Setting> {
 
 
+  
   @override
   Widget build(BuildContext context) {
+    _buildDR() {
+      return Padding(
+        padding: const EdgeInsets.only(top: 10, bottom: 10),
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: Colors.grey.shade500, width: 1))),
+          child: const Center(heightFactor: 1.5),
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Container(
-         child: ListView(
-  padding: const EdgeInsets.all(8),
-  children: <Widget>[
-    Container(
-      height: 50,
-      color: Colors.amber[600],
-      child: const Center(child: Text('Entry A')),
-    ),
-    Container(
-      height: 50,
-      color: Colors.amber[500],
-      child: const Center(child: Text('Entry B')),
-    ),
-    Container(
-      height: 50,
-      color: Colors.amber[100],
-      child: const Center(child: Text('Entry C')),
-    ),
-  ],
-),
-         ),
+      backgroundColor: Colors.grey.shade200,
+      body: ListView(
+        children: [
+          Container(
+            color: Colors.blueAccent,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'images/logo.png',
+                    cacheHeight: 60,
+                    cacheWidth: 270,
+                  ),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.search, size: 45)),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.circle, size: 45)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+         Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:[
+                  Container(
+                    padding: EdgeInsets.all(3.0),
+                    child: Material(
+                      child: ListTile(
+                        title: const Text('Thông tin cá nhân'),
+                         onLongPress: (){},
+                      ),
+                    ),
+                  ),
+                      Container(
+                    padding: EdgeInsets.all(3.0),
+                    child: Material(
+                      child: ListTile(
+                        title: const Text('Tài khoản và mật khẩu'),
+                         onLongPress: (){},
+                      ),
+                    ),
+                  ),
+                      Container(
+                    padding: EdgeInsets.all(3.0),
+                    child: Material(
+                      child: ListTile(
+                        title: const Text('Chế độ tối'),
+                         onLongPress: (){},
+                      ),
+                    ),
+                  ),
+                      Container(
+                    padding: EdgeInsets.all(3.0),
+                    child: Material(
+                      child: ListTile(
+                        title: const Text('Liên hệ'),
+                         onLongPress: (){},
+                      ),
+                    ),
+                  ),
+                      Container(
+                    padding: EdgeInsets.all(3.0),
+                    child: Material(
+                      child: ListTile(
+                        title: const Text('Đăng xuất'),
+                         onLongPress: (){},
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
- 
 }
