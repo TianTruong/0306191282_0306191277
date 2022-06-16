57c522565337ca4dacbb783f3e69a7164656c939
pxthinh.vn@gmail.com <pxthinh.vn@gmail.com>
2022-06-16 09:35:51 +0700

    khoa nhiet lanh

--- src/app_tin_tuc_cao_thang/lib/main.dart
+++ src/app_tin_tuc_cao_thang/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:app_tin_tuc_cao_thang/home/khoa/bomonkinhte.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:app_tin_tuc_cao_thang/login/sign_in.dart';
-import 'package:app_tin_tuc_cao_thang/home/khoa/ckdl.dart';
+import 'package:app_tin_tuc_cao_thang/home/khoa/nhietlanh.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CKDL(),
+      home: const NhietLanh(),
     );
   }
 }
